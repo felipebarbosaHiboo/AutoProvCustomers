b07d0c299a1b810a0dfbf89dcca01b9ac5319d43
felipebarbosaHiboo <felipebarbosa@hiboonetoworks.com>
2024-07-19 14:50:23 -0400

    Bootstrap more or less

--- service_app/service_app/forms.py
+++ service_app/service_app/forms.py
@@ -1,12 +1,33 @@
+import re
+
 from django import forms
+from django.core.exceptions import ValidationError
+
+from django.core.exceptions import ValidationError
+import re
 
 class BootstrapMixin:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.fields = None
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
             if field_name == 'cbs':
                 field.widget.attrs['readonly'] = True
+
+    def add_error(self, field, error):
+        super().add_error(field, error)
+        self.fields[field].widget.attrs['class'] += ' is-invalid'
+
+def validate_ipv4_with_mask(value):
+    pattern = re.compile(r'^(\d{1,3}\.){3}\d{1,3}\/\d{1,2}$')
+    if not pattern.match(value):
+        raise ValidationError('Enter a valid IPv4 address with a mask (e.g., 192.168.0.1/24).')
+
+def validate_ipv6_with_mask(value):
+    pattern = re.compile(r'^([0-9a-fA-F:]+)\/\d{1,3}$')
+    if not pattern.match(value):
+        raise ValidationError('Enter a valid IPv6 address with a mask (e.g., 2001:db8::/32).')
 
 SERVICE_CHOICES = (
     ("0", "SELECT ONE AND CLICK SAVE"),
@@ -35,10 +56,10 @@ CIR_CHOICES = [
     ("100g", "100 Gbps"),
 ]
 
-class CustomerForm(forms.Form):
+class CustomerForm(forms.Form, BootstrapMixin):
     service_Type = forms.ChoiceField(choices=SERVICE_CHOICES)
 
-class DiaDynamicServiceForm(forms.Form):
+class DiaDynamicServiceForm(forms.Form, BootstrapMixin):
     cust_id = forms.IntegerField(max_value=999999, min_value=100000, label="Customer ID", initial=123456)
     svc_id = forms.IntegerField(max_value=9999, min_value=1000, label="Service ID", initial=1234)
     ifd = forms.CharField(label="Interface Name", initial="xe-0/0/0")
@@ -62,17 +83,17 @@ class DiaStaticBgpWithVlanIdServiceForm(forms.Form, BootstrapMixin):
     ifd = forms.CharField(label="Interface Name", initial="xe-0/0/0")
     vlan_id = forms.IntegerField(max_value=4096, min_value=1, label="VLAN ID", initial=1245)
     ifu = forms.IntegerField(label="Unit Number", initial=0)
-    prefix_ipv4_01 = forms.GenericIPAddressField(protocol='IPv4', label="IPv4 Network/Mask 1", initial="10.10.10.10/8")
-    prefix_ipv4_02 = forms.GenericIPAddressField(protocol='IPv4', label="IPv4 Network/Mask 2", initial="10.10.10.10/8")
-    addr_ipv4_pe = forms.GenericIPAddressField(protocol='IPv4', label="IPv4 Address/Mask PE", initial="10.10.10.10/8")
-    addr_ipv4_pe_rpm = forms.GenericIPAddressField(protocol='IPv4', label="IPv4 Address PE RPM", initial="10.10.10.10/8")
-    addr_ipv4_ce1 = forms.GenericIPAddressField(protocol='IPv4', label="IPv4 Address CE1", initial="10.10.10.10/8")
-    addr_ipv4_ce2 = forms.GenericIPAddressField(protocol='IPv4', label="IPv4 Address CE2", initial="10.10.10.10/8")
-    prefix_ipv6 = forms.GenericIPAddressField(protocol='IPv6', label="IPv6 Network/Mask", initial="2001:db8::/32")
-    addr_ipv6_pe = forms.GenericIPAddressField(protocol='IPv6', label="IPv6 Address/Mask PE", initial="2001:db8::1/32")
-    addr_ipv6_pe_rpm = forms.GenericIPAddressField(protocol='IPv6', label="IPv6 Address PE RPM", initial="2001:db8::2/32")
-    addr_ipv6_ce1 = forms.GenericIPAddressField(protocol='IPv6', label="IPv6 Address CE1", initial="2001:db8::3/32")
-    addr_ipv6_ce2 = forms.GenericIPAddressField(protocol='IPv6', label="IPv6 Address CE2", initial="2001:db8::4/32")
+    prefix_ipv4_01 = forms.CharField(label="IPv4 Network/Mask 1", initial="10.10.10.10/8", validators=[validate_ipv4_with_mask])
+    prefix_ipv4_02 = forms.CharField(label="IPv4 Network/Mask 2", initial="10.10.10.10/8", validators=[validate_ipv4_with_mask])
+    addr_ipv4_pe = forms.CharField(label="IPv4 Address/Mask PE", initial="10.10.10.10/8", validators=[validate_ipv4_with_mask])
+    addr_ipv4_pe_rpm = forms.CharField(label="IPv4 Address PE RPM", initial="10.10.10.10/8", validators=[validate_ipv4_with_mask])
+    addr_ipv4_ce1 = forms.CharField(label="IPv4 Address CE1", initial="10.10.10.10/8", validators=[validate_ipv4_with_mask])
+    addr_ipv4_ce2 = forms.CharField(label="IPv4 Address CE2", initial="10.10.10.10/8", validators=[validate_ipv4_with_mask])
+    prefix_ipv6 = forms.CharField(label="IPv6 Address/Mask PE", initial="2001:db8::1/32", validators=[validate_ipv6_with_mask])
+    addr_ipv6_pe = forms.CharField(label="IPv6 Address/Mask PE", initial="2001:db8::1/32", validators=[validate_ipv6_with_mask])
+    addr_ipv6_pe_rpm = forms.CharField(label="IPv6 Address/Mask PE", initial="2001:db8::1/32", validators=[validate_ipv6_with_mask])
+    addr_ipv6_ce1 = forms.CharField(label="IPv6 Address/Mask PE", initial="2001:db8::1/32", validators=[validate_ipv6_with_mask])
+    addr_ipv6_ce2 = forms.CharField(label="IPv6 Address/Mask PE", initial="2001:db8::1/32", validators=[validate_ipv6_with_mask])
     cir = forms.ChoiceField(choices=CIR_CHOICES, label="Committed Information Rate")
     cbs = forms.IntegerField(label="Committed Burst Size", required=False)
     auth_key = forms.CharField(label="Authentication Key", initial="Key negotiated with customer")
@@ -85,17 +106,17 @@ class DiaStaticBgpWithVlanIdServiceForm(forms.Form, BootstrapMixin):
             cleaned_data["cbs"] = cir_value * 0.1
         return cleaned_data
 
-class DiaStaticBgpServiceForm(forms.Form):
+class DiaStaticBgpServiceForm(forms.Form, BootstrapMixin):
     cust_id = forms.IntegerField(max_value=999999, min_value=100000, label="Customer ID", initial=123456)
     svc_id = forms.IntegerField(max_value=9999, min_value=1000, label="Service ID", initial=1234)
     cust_asn = forms.IntegerField(label="Autonomous System Number", initial=65000)
     ifd = forms.CharField(label="Interface Name", initial="xe-0/0/0")
-    prefix_ipv4 = forms.GenericIPAddressField(protocol='IPv4', label="IPv4 Network/Mask", initial="10.10.10.10/8")
-    addr_ipv4_pe = forms.GenericIPAddressField(protocol='IPv4', label="IPv4 Address/Mask PE", initial="10.10.10.10/8")
-    addr_ipv4_ce = forms.GenericIPAddressField(protocol='IPv4', label="IPv4 Address CE", initial="10.10.10.10/8")
-    prefix_ipv6 = forms.GenericIPAddressField(protocol='IPv6', label="IPv6 Network/Mask", initial="2001:db8::/32")
-    addr_ipv6_pe = forms.GenericIPAddressField(protocol='IPv6', label="IPv6 Address/Mask PE", initial="2001:db8::1/32")
-    addr_ipv6_ce = forms.GenericIPAddressField(protocol='IPv6', label="IPv6 Address CE", initial="2001:db8::2/32")
+    prefix_ipv4 = forms.CharField(label="IPv4 Network/Mask", initial="10.10.10.10/8", validators=[validate_ipv4_with_mask])
+    addr_ipv4_pe = forms.CharField(label="IPv4 Address/Mask PE", initial="10.10.10.10/8", validators=[validate_ipv4_with_mask])
+    addr_ipv4_ce = forms.CharField(label="IPv4 Address CE", initial="10.10.10.10/8", validators=[validate_ipv4_with_mask])
+    prefix_ipv6 = forms.CharField(label="IPv6 Address/Mask PE", initial="2001:db8::1/32", validators=[validate_ipv6_with_mask])
+    addr_ipv6_pe = forms.CharField(label="IPv6 Address/Mask PE", initial="2001:db8::1/32", validators=[validate_ipv6_with_mask])
+    addr_ipv6_ce = forms.CharField(label="IPv6 Address/Mask PE", initial="2001:db8::1/32", validators=[validate_ipv6_with_mask])
     cir = forms.ChoiceField(choices=CIR_CHOICES, label="Committed Information Rate")
     cbs = forms.IntegerField(label="Committed Burst Size", required=False)
 
@@ -107,16 +128,16 @@ class DiaStaticBgpServiceForm(forms.Form):
             cleaned_data["cbs"] = cir_value * 0.1
         return cleaned_data
 
-class DiaStaticServiceForm(forms.Form):
+class DiaStaticServiceForm(forms.Form, BootstrapMixin):
     cust_id = forms.IntegerField(max_value=999999, min_value=100000, label="Customer ID", initial=123456)
     svc_id = forms.IntegerField(max_value=9999, min_value=1000, label="Service ID", initial=1234)
     ifd = forms.CharField(label="Interface Name", initial="xe-0/0/0")
-    prefix_ipv4 = forms.GenericIPAddressField(protocol='IPv4', label="IPv4 Network/Mask", initial="10.10.10.10/8")
-    addr_ipv4_pe = forms.GenericIPAddressField(protocol='IPv4', label="IPv4 Address/Mask PE", initial="10.10.10.10/8")
-    addr_ipv4_ce = forms.GenericIPAddressField(protocol='IPv4', label="IPv4 Address CE", initial="10.10.10.10/8")
-    prefix_ipv6 = forms.GenericIPAddressField(protocol='IPv6', label="IPv6 Network/Mask", initial="2001:db8::/32")
-    addr_ipv6_pe = forms.GenericIPAddressField(protocol='IPv6', label="IPv6 Address/Mask PE", initial="2001:db8::1/32")
-    addr_ipv6_ce = forms.GenericIPAddressField(protocol='IPv6', label="IPv6 Address CE", initial="2001:db8::2/32")
+    prefix_ipv4 = forms.CharField(label="IPv4 Network/Mask", initial="10.10.10.10/8", validators=[validate_ipv4_with_mask])
+    addr_ipv4_pe = forms.CharField(label="IPv4 Address/Mask PE", initial="10.10.10.10/8", validators=[validate_ipv4_with_mask])
+    addr_ipv4_ce = forms.CharField(label="IPv4 Address CE", initial="10.10.10.10/8", validators=[validate_ipv4_with_mask])
+    prefix_ipv6 = forms.CharField(label="IPv6 Address/Mask PE", initial="2001:db8::1/32", validators=[validate_ipv6_with_mask])
+    addr_ipv6_pe = forms.CharField(label="IPv6 Address/Mask PE", initial="2001:db8::1/32", validators=[validate_ipv6_with_mask])
+    addr_ipv6_ce = forms.CharField(label="IPv6 Address/Mask PE", initial="2001:db8::1/32", validators=[validate_ipv6_with_mask])
     cir = forms.ChoiceField(choices=CIR_CHOICES, label="Committed Information Rate")
     cbs = forms.IntegerField(label="Committed Burst Size", required=False)
 
@@ -128,7 +149,7 @@ class DiaStaticServiceForm(forms.Form):
             cleaned_data["cbs"] = cir_value * 0.1
         return cleaned_data
 
-class L2vpnElanServiceForm(forms.Form):
+class L2vpnElanServiceForm(forms.Form, BootstrapMixin):
     cust_id = forms.IntegerField(max_value=999999, min_value=100000, label="Customer ID", initial=123456)
     svc_id = forms.IntegerField(max_value=9999, min_value=1000, label="Service ID", initial=1234)
     ifd = forms.CharField(label="Interface Name", initial="xe-0/0/0")
@@ -146,7 +167,7 @@ class L2vpnElanServiceForm(forms.Form):
             cleaned_data["cbs"] = cir_value * 0.1
         return cleaned_data
 
-class L2vpnElineServiceForm(forms.Form):
+class L2vpnElineServiceForm(forms.Form, BootstrapMixin):
     cust_id = forms.IntegerField(max_value=999999, min_value=100000, label="Customer ID", initial=123456)
     svc_id = forms.IntegerField(max_value=9999, min_value=1000, label="Service ID", initial=1234)
     ifd = forms.CharField(label="Interface Name", initial="xe-0/0/0")
@@ -170,17 +191,17 @@ class L2vpnElineServiceForm(forms.Form):
 
         return cleaned_data
 
-class L3vpnBgpServiceForm(forms.Form):
+class L3vpnBgpServiceForm(forms.Form, BootstrapMixin):
     cust_id = forms.IntegerField(max_value=999999, min_value=100000, label="Customer ID", initial=123456)
     svc_id = forms.IntegerField(max_value=9999, min_value=1000, label="Service ID", initial=1234)
     cust_asn = forms.IntegerField(label="Autonomous System Number", initial=65000)
     ifd = forms.CharField(label="Interface Name", initial="xe-0/0/0")
-    prefix_ipv4 = forms.GenericIPAddressField(protocol='IPv4', label="IPv4 Network/Mask", initial="10.10.10.10/8")
-    addr_ipv4_pe = forms.GenericIPAddressField(protocol='IPv4', label="IPv4 Address/Mask PE", initial="10.10.10.10/8")
-    addr_ipv4_ce = forms.GenericIPAddressField(protocol='IPv4', label="IPv4 Address CE", initial="10.10.10.10/8")
+    prefix_ipv4 = forms.CharField(label="IPv4 Network/Mask", initial="10.10.10.10/8", validators=[validate_ipv4_with_mask])
+    addr_ipv4_pe = forms.CharField(label="IPv4 Address/Mask PE", initial="10.10.10.10/8", validators=[validate_ipv4_with_mask])
+    addr_ipv4_ce = forms.CharField(label="IPv4 Address CE", initial="10.10.10.10/8", validators=[validate_ipv4_with_mask])
     prefix_ipv6 = forms.GenericIPAddressField(protocol='IPv6', label="IPv6 Network/Mask", initial="2001:db8::/32")
-    addr_ipv6_pe = forms.GenericIPAddressField(protocol='IPv6', label="IPv6 Address/Mask PE", initial="2001:db8::1/32")
-    addr_ipv6_ce = forms.GenericIPAddressField(protocol='IPv6', label="IPv6 Address CE", initial="2001:db8::2/32")
+    addr_ipv6_pe = forms.CharField(label="IPv6 Address/Mask PE", initial="2001:db8::1/32", validators=[validate_ipv6_with_mask])
+    addr_ipv6_ce = forms.CharField(label="IPv6 Address/Mask PE", initial="2001:db8::1/32", validators=[validate_ipv6_with_mask])
     cir = forms.ChoiceField(choices=CIR_CHOICES, label="Committed Information Rate")
     cbs = forms.IntegerField(label="Committed Burst Size", required=False)
 
@@ -192,16 +213,16 @@ class L3vpnBgpServiceForm(forms.Form):
             cleaned_data["cbs"] = cir_value * 0.1
         return cleaned_data
 
-class L3vpnStaticServiceForm(forms.Form):
+class L3vpnStaticServiceForm(forms.Form, BootstrapMixin):
     cust_id = forms.IntegerField(max_value=999999, min_value=100000, label="Customer ID", initial=123456)
     svc_id = forms.IntegerField(max_value=9999, min_value=1000, label="Service ID", initial=1234)
     ifd = forms.CharField(label="Interface Name", initial="xe-0/0/0")
-    prefix_ipv4 = forms.GenericIPAddressField(protocol='IPv4', label="IPv4 Network/Mask", initial="10.10.10.10/8")
-    addr_ipv4_pe = forms.GenericIPAddressField(protocol='IPv4', label="IPv4 Address/Mask PE", initial="10.10.10.10/8")
-    addr_ipv4_ce = forms.GenericIPAddressField(protocol='IPv4', label="IPv4 Address CE", initial="10.10.10.10/8")
-    prefix_ipv6 = forms.GenericIPAddressField(protocol='IPv6', label="IPv6 Network/Mask", initial="2001:db8::/32")
-    addr_ipv6_pe = forms.GenericIPAddressField(protocol='IPv6', label="IPv6 Address/Mask PE", initial="2001:db8::1/32")
-    addr_ipv6_ce = forms.GenericIPAddressField(protocol='IPv6', label="IPv6 Address CE", initial="2001:db8::2/32")
+    prefix_ipv4 = forms.CharField(label="IPv4 Network/Mask", initial="10.10.10.10/8", validators=[validate_ipv4_with_mask])
+    addr_ipv4_pe = forms.CharField(label="IPv4 Address/Mask PE", initial="10.10.10.10/8", validators=[validate_ipv4_with_mask])
+    addr_ipv4_ce = forms.CharField(label="IPv4 Address CE", initial="10.10.10.10/8", validators=[validate_ipv4_with_mask])
+    prefix_ipv6 = forms.CharField(label="IPv6 Address/Mask PE", initial="2001:db8::1/32", validators=[validate_ipv6_with_mask])
+    addr_ipv6_pe = forms.CharField(label="IPv6 Address/Mask PE", initial="2001:db8::1/32", validators=[validate_ipv6_with_mask])
+    addr_ipv6_ce = forms.CharField(label="IPv6 Address/Mask PE", initial="2001:db8::1/32", validators=[validate_ipv6_with_mask])
     cir = forms.ChoiceField(choices=CIR_CHOICES, label="Committed Information Rate")
     cbs = forms.IntegerField(label="Committed Burst Size", required=False)
 
--- service_app/service_app/urls.py
+++ service_app/service_app/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import customer, get_additional_fields
 
 urlpatterns = [
+    path('', customer, name='home'),  # Home page pointing to the customer form view
     path('customer/', customer, name='customer'),
     path('get-additional-fields/', get_additional_fields, name='get_additional_fields'),
 ]
