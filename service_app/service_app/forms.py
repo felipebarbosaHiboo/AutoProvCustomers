bca890b269528cd371b18ee82bb51d4592969ec1
felipebarbosaHiboo <felipebarbosa@hiboonetoworks.com>
2024-07-19 13:20:16 -0400

    Customer Template, getting new fields based on service type

--- service_app/service_app/forms.py
+++ service_app/service_app/forms.py
@@ -1,13 +1,16 @@
 from django import forms
 
 SERVICE_CHOICES = (
-    ("1", "DIA STATIC BGP WITH VLAN_ID"),
-    ("2", "DIA STATIC BGP"),
-    ("3", "DIA STATIC"),
-    ("4", "L2VPN ELAN"),
-    ("5", "L2VPN ELINE"),
-    ("6", "L3VPN BGP"),
-    ("7", "L3VPN STATIC"),
+    ("0", "SELECT ONE AND CLICK SAVE"),
+    ("0", ""),
+    ("1", "DIA DYNAMIC"),
+    ("2", "DIA STATIC BGP WITH VLAN_ID"),
+    ("3", "DIA STATIC BGP"),
+    ("4", "DIA STATIC"),
+    ("5", "L2VPN ELAN"),
+    ("6", "L2VPN ELINE"),
+    ("7", "L3VPN BGP"),
+    ("8", "L3VPN STATIC"),
 )
 
 class CustomerForm(forms.Form):
@@ -15,10 +18,109 @@ class CustomerForm(forms.Form):
     service_ID = forms.IntegerField()
     service_Type = forms.ChoiceField(choices=SERVICE_CHOICES)
 
-class DiaStaticBgpWithVlanId_ServiceForm(forms.Form):
-    interfaceName = forms.CharField()
-    svlan = forms.IntegerField(max_value=4096)
-    dataCenter = forms.CharField()
-    cir = forms.IntegerField()
-    cbs = forms.IntegerField()
+class DiaDynamicServiceForm(forms.Form):
+    cust_id = forms.IntegerField(max_value=999999, min_value=100000, label="Customer ID")
+    svc_id = forms.IntegerField(max_value=9999, min_value=1000, label="Service ID")
+    ifd = forms.CharField(label="Interface Name")
+    svlan = forms.IntegerField(max_value=4096, min_value=1, label="Service VLAN ID")
+    pwht_dc = forms.ChoiceField(choices=[('dc01', 'DC01'), ('dc02', 'DC02')], label="Data Center")
+    cir = forms.IntegerField(label="Committed Information Rate")
+    cbs = forms.IntegerField(label="Committed Burst Size")
 
+class DiaStaticBgpWithVlanIdServiceForm(forms.Form):
+    cust_id = forms.IntegerField(max_value=999999, min_value=100000, label="Customer ID")
+    svc_id = forms.IntegerField(max_value=9999, min_value=1000, label="Service ID")
+    cust_asn = forms.IntegerField(label="Autonomous System Number")
+    ifd = forms.CharField(label="Interface Name")
+    vlan_id = forms.IntegerField(max_value=4096, min_value=1, label="VLAN ID")
+    ifu = forms.IntegerField(label="Unit Number")
+    prefix_ipv4_01 = forms.CharField(label="IPv4 Network/Mask 1")
+    prefix_ipv4_02 = forms.CharField(label="IPv4 Network/Mask 2")
+    addr_ipv4_pe = forms.CharField(label="IPv4 Address/Mask PE")
+    addr_ipv4_pe_rpm = forms.CharField(label="IPv4 Address PE RPM")
+    addr_ipv4_ce1 = forms.CharField(label="IPv4 Address CE1")
+    addr_ipv4_ce2 = forms.CharField(label="IPv4 Address CE2")
+    prefix_ipv6 = forms.CharField(label="IPv6 Network/Mask")
+    addr_ipv6_pe = forms.CharField(label="IPv6 Address/Mask PE")
+    addr_ipv6_pe_rpm = forms.CharField(label="IPv6 Address PE RPM")
+    addr_ipv6_ce1 = forms.CharField(label="IPv6 Address CE1")
+    addr_ipv6_ce2 = forms.CharField(label="IPv6 Address CE2")
+    cir = forms.IntegerField(label="Committed Information Rate")
+    cbs = forms.IntegerField(label="Committed Burst Size")
+    auth_key = forms.CharField(label="Authentication Key")
+
+
+class DiaStaticBgpServiceForm(forms.Form):
+    cust_id = forms.IntegerField(max_value=999999, min_value=100000, label="Customer ID")
+    svc_id = forms.IntegerField(max_value=9999, min_value=1000, label="Service ID")
+    cust_asn = forms.IntegerField(label="Autonomous System Number")
+    ifd = forms.CharField(label="Interface Name")
+    prefix_ipv4 = forms.CharField(label="IPv4 Network/Mask")
+    addr_ipv4_pe = forms.CharField(label="IPv4 Address/Mask PE")
+    addr_ipv4_ce = forms.CharField(label="IPv4 Address CE")
+    prefix_ipv6 = forms.CharField(label="IPv6 Network/Mask")
+    addr_ipv6_pe = forms.CharField(label="IPv6 Address/Mask PE")
+    addr_ipv6_ce = forms.CharField(label="IPv6 Address CE")
+    cir = forms.IntegerField(label="Committed Information Rate")
+    cbs = forms.IntegerField(label="Committed Burst Size")
+
+class DiaStaticServiceForm(forms.Form):
+    cust_id = forms.IntegerField(max_value=999999, min_value=100000, label="Customer ID")
+    svc_id = forms.IntegerField(max_value=9999, min_value=1000, label="Service ID")
+    ifd = forms.CharField(label="Interface Name")
+    prefix_ipv4 = forms.CharField(label="IPv4 Network/Mask")
+    addr_ipv4_pe = forms.CharField(label="IPv4 Address/Mask PE")
+    addr_ipv4_ce = forms.CharField(label="IPv4 Address CE")
+    prefix_ipv6 = forms.CharField(label="IPv6 Network/Mask")
+    addr_ipv6_pe = forms.CharField(label="IPv6 Address/Mask PE")
+    addr_ipv6_ce = forms.CharField(label="IPv6 Address CE")
+    cir = forms.IntegerField(label="Committed Information Rate")
+    cbs = forms.IntegerField(label="Committed Burst Size")
+
+class L2vpnElanServiceForm(forms.Form):
+    cust_id = forms.IntegerField(max_value=999999, min_value=100000, label="Customer ID")
+    svc_id = forms.IntegerField(max_value=9999, min_value=1000, label="Service ID")
+    ifd = forms.CharField(label="Interface Name")
+    mtu = forms.ChoiceField(choices=[('1522', '1522'), ('9216', '9216')], label="MTU")
+    vpls_pref = forms.ChoiceField(choices=[('primary', 'Primary'), ('backup', 'Backup')], label="VPLS Multi-Homing Preference")
+    vpls_site_id = forms.IntegerField(label="VPLS Site Identifier")
+    cir = forms.IntegerField(label="Committed Information Rate")
+    cbs = forms.IntegerField(label="Committed Burst Size")
+
+
+class L2vpnElineServiceForm(forms.Form):
+    cust_id = forms.IntegerField(max_value=999999, min_value=100000, label="Customer ID")
+    svc_id = forms.IntegerField(max_value=9999, min_value=1000, label="Service ID")
+    ifd = forms.CharField(label="Interface Name")
+    mtu = forms.ChoiceField(choices=[('1522', '1522'), ('9216', '9216')], label="MTU")
+    vpws_id_local = forms.IntegerField(label="Local VPWS Circuit ID")
+    vpws_id_remote = forms.IntegerField(label="Remote VPWS Circuit ID")
+    cir = forms.IntegerField(label="Committed Information Rate")
+    cbs = forms.IntegerField(label="Committed Burst Size")
+
+class L3vpnBgpServiceForm(forms.Form):
+    cust_id = forms.IntegerField(max_value=999999, min_value=100000, label="Customer ID")
+    svc_id = forms.IntegerField(max_value=9999, min_value=1000, label="Service ID")
+    cust_asn = forms.IntegerField(label="Autonomous System Number")
+    ifd = forms.CharField(label="Interface Name")
+    prefix_ipv4 = forms.CharField(label="IPv4 Network/Mask")
+    addr_ipv4_pe = forms.CharField(label="IPv4 Address/Mask PE")
+    addr_ipv4_ce = forms.CharField(label="IPv4 Address CE")
+    prefix_ipv6 = forms.CharField(label="IPv6 Network/Mask")
+    addr_ipv6_pe = forms.CharField(label="IPv6 Address/Mask PE")
+    addr_ipv6_ce = forms.CharField(label="IPv6 Address CE")
+    cir = forms.IntegerField(label="Committed Information Rate")
+    cbs = forms.IntegerField(label="Committed Burst Size")
+
+class L3vpnStaticServiceForm(forms.Form):
+    cust_id = forms.IntegerField(max_value=999999, min_value=100000, label="Customer ID")
+    svc_id = forms.IntegerField(max_value=9999, min_value=1000, label="Service ID")
+    ifd = forms.CharField(label="Interface Name")
+    prefix_ipv4 = forms.CharField(label="IPv4 Network/Mask")
+    addr_ipv4_pe = forms.CharField(label="IPv4 Address/Mask PE")
+    addr_ipv4_ce = forms.CharField(label="IPv4 Address CE")
+    prefix_ipv6 = forms.CharField(label="IPv6 Network/Mask")
+    addr_ipv6_pe = forms.CharField(label="IPv6 Address/Mask PE")
+    addr_ipv6_ce = forms.CharField(label="IPv6 Address CE")
+    cir = forms.IntegerField(label="Committed Information Rate")
+    cbs = forms.IntegerField(label="Committed Burst Size")
--- service_app/service_app/views.py
+++ service_app/service_app/views.py
@@ -1,21 +1,41 @@
+#views.py
+
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.template.loader import render_to_string
 
-from .forms import CustomerForm, DiaStaticBgpWithVlanId_ServiceForm
-
-# Create your views here.
+from .forms import (
+    DiaStaticBgpWithVlanIdServiceForm, DiaStaticBgpServiceForm, DiaStaticServiceForm,
+    L2vpnElanServiceForm, L2vpnElineServiceForm, L3vpnBgpServiceForm, L3vpnStaticServiceForm, DiaDynamicServiceForm,
+    CustomerForm
+)
 
 def get_additional_fields(request):
     service_type = request.GET.get('service_type')
     html = ''
-    if service_type == "1":  # DIA STATIC BGP WITH VLAN_ID
-        form = DiaStaticBgpWithVlanId_ServiceForm()
-        html = render_to_string('additional_fields.html', {'form': form})
+    if service_type == "1":  # DIA DYNAMIC
+        form = DiaDynamicServiceForm()
+    elif service_type == "2":  # DIA STATIC BGP WITH VLAN_ID
+        form = DiaStaticBgpWithVlanIdServiceForm()
+    elif service_type == "3":  # DIA STATIC BGP
+        form = DiaStaticBgpServiceForm()
+    elif service_type == "4":  # DIA STATIC
+        form = DiaStaticServiceForm()
+    elif service_type == "5":  # L2VPN ELAN
+        form = L2vpnElanServiceForm()
+    elif service_type == "6":  # L2VPN ELINE
+        form = L2vpnElineServiceForm()
+    elif service_type == "7":  # L3VPN BGP
+        form = L3vpnBgpServiceForm()
+    elif service_type == "8":  # L3VPN STATIC
+        form = L3vpnStaticServiceForm()
+
+    html = render_to_string('additional_fields.html', {'form': form})
     return JsonResponse({'html': html})
+
 def customer(request):
     customer_form = CustomerForm(request.POST or None)
-    dia_static_bgp_with_vlanid_form = None
+    additional_form = None  # Initialize the variable
 
     if request.method == 'POST':
         if customer_form.is_valid():
@@ -26,15 +46,29 @@ def customer(request):
             service_Type = customer_form.cleaned_data['service_Type']
             print('Service Type', service_Type)
 
-            if service_Type == "1":  # DIA STATIC BGP WITH VLAN_ID
-                dia_static_bgp_with_vlanid_form = DiaStaticBgpWithVlanId_ServiceForm(request.POST)
-                if dia_static_bgp_with_vlanid_form.is_valid():
-                    # Process DiaStaticBgpWithVlanId_ServiceForm data
-                    print('Additional Form Data:', dia_static_bgp_with_vlanid_form.cleaned_data)
+            if service_Type == "1":  # DIA DYNAMIC
+                additional_form = DiaDynamicServiceForm(request.POST)
+            elif service_Type == "2":  # DIA STATIC BGP WITH VLAN_ID
+                additional_form = DiaStaticBgpWithVlanIdServiceForm(request.POST)
+            elif service_Type == "3":  # DIA STATIC BGP
+                additional_form = DiaStaticBgpServiceForm(request.POST)
+            elif service_Type == "4":  # DIA STATIC
+                additional_form = DiaStaticServiceForm(request.POST)
+            elif service_Type == "5":  # L2VPN ELAN
+                additional_form = L2vpnElanServiceForm(request.POST)
+            elif service_Type == "6":  # L2VPN ELINE
+                additional_form = L2vpnElineServiceForm(request.POST)
+            elif service_Type == "7":  # L3VPN BGP
+                additional_form = L3vpnBgpServiceForm(request.POST)
+            elif service_Type == "8":  # L3VPN STATIC
+                additional_form = L3vpnStaticServiceForm(request.POST)
+
+            if additional_form and additional_form.is_valid():
+                # Process additional form data
+                print('Additional Form Data:', additional_form.cleaned_data)
 
     context = {
         'customer_form': customer_form,
-        'dia_static_bgp_with_vlanid_form': dia_static_bgp_with_vlanid_form,
+        'additional_form': additional_form,
     }
     return render(request, "forms.html", context)
-
